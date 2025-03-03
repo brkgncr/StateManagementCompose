d8a5fdc1aa660247529a23aca7009b91d2d1d682
Burak Gençer <bggithub@proton.me>
2025-03-03 23:33:18 +0300

    Added image and test

--- app/src/main/java/com/burak/statemanagementcompose/MainActivity.kt
+++ app/src/main/java/com/burak/statemanagementcompose/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -17,6 +19,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ComposableTarget
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.burak.statemanagementcompose.ui.theme.StateManagementComposeTheme
@@ -54,7 +62,18 @@ fun MainScreen() {
         }, enabled = true) {
             Text("Example Button")
         }
-
+        Spacer(modifier = Modifier.padding(10.dp))
+        Image(bitmap = ImageBitmap.imageResource(id = R.drawable.istanbul),
+            contentDescription = "istanbul",
+            modifier = Modifier.size(250.dp, 200.dp))
+        Spacer(modifier = Modifier.padding(10.dp))
+        Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "test")
+        Spacer(modifier = Modifier.padding(10.dp))
+        Image(painter = ColorPainter(Color.Red),
+            contentDescription = "red",
+            modifier = Modifier.size(100.dp,100.dp)
+        )
     }
     
 }
