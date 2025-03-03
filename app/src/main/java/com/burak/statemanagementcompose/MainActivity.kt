ba1e3b4d003d27526d26a58387b023021a9eb14f
Burak Gençer <bggithub@proton.me>
2025-03-03 22:56:12 +0300

    Added Button and test

--- app/src/main/java/com/burak/statemanagementcompose/MainActivity.kt
+++ app/src/main/java/com/burak/statemanagementcompose/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposableTarget
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.burak.statemanagementcompose.ui.theme.StateManagementComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +28,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             StateManagementComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        MainScreen()
+                    }
                 }
             }
         }
@@ -31,17 +38,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Text(text = "Hello Android")
+        Spacer(modifier = Modifier.padding(10.dp))
+        Button(onClick = {
+            println("click button")
+        }, enabled = true) {
+            Text("Example Button")
+        }
+
+    }
+    
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     StateManagementComposeTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
